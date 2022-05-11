e4112d6dde0c9b6acbece11899b1274b7a4f4cc8
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-11 19:21:38 -0400

    Commit 6 - Yell at the Ninja Turtles answered

--- app.js
+++ app.js
@@ -29,28 +29,34 @@
 // }
 
 // Getting to know you
-const kenny = ["Kenny", 1000, "Austin"];
-const jimHaff = ["Jim H", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const jimClark = ["Jim C", 186, "LA"];
-const ryan = ["Ryan", 65, "Denver"];
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
 
-kenny[0] = 'Gameboy'
-console.log(kenny);
+// kenny[0] = 'Gameboy'
+// console.log(kenny);
 
-jimClark[1] = jimClark[1]+1
-console.log(jimClark);
+// jimClark[1] = jimClark[1]+1
+// console.log(jimClark);
 
-ryan[2] = 'Gotham City'
-console.log(ryan);
+// ryan[2] = 'Gotham City'
+// console.log(ryan);
 
-reuben.pop();
-console.log(reuben);
-reuben.push('Chicago');
-console.log(reuben);
+// reuben.pop();
+// console.log(reuben);
+// reuben.push('Chicago');
+// console.log(reuben);
 
-jimHaff.splice(2, 1,'Indy','Chicago','Nashville');
-console.log(jimHaff)
+// jimHaff.splice(2, 1,'Indy','Chicago','Nashville');
+// console.log(jimHaff)
 
-jimHaff.splice(2,2);
-console.log(jimHaff);
+// jimHaff.splice(2,2);
+// console.log(jimHaff);
+
+const turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+for (let i = 0; i < turtles.length; i++){
+    turtles[i] = turtles[i].toUpperCase();
+    console.log(turtles[i]);
+}
