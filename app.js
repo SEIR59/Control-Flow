659a50d7436066ba5d6e30293f18bdf4a4c5dd61
Freightscape <jordan@freightscape.net>
2022-05-11 19:08:37 -0400

    Commit 1 - Easy Going Answered

---
+++ app.js
@@ -0,0 +1,4 @@
+//Creating a for loop of 1 through 20
+for (let i = 1; i <= 20; i++) {
+    console.log(i)
+};
---
+++ conditional_practice.js
@@ -0,0 +1,14 @@
+const sunny = true;
+
+const temp = 76;
+
+console.log(sunny, temp);
+
+const sunny = !true;
+console.log(sunny);
+
+const notSunny = false;
+console.log(notSunny);
+
+const notSunny =!false;
+console.log(notSunny)
