853592cc0c345affef43b8e9a8b52cf1901b4b79
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-05-11 17:12:40 -0400

    Commit 1 - Easy Going answered

---
+++ app.js
@@ -0,0 +1,26 @@
+// 1 - Easy Going
+for (let i = 1; i <= 20; i++) {
+    console.log(i)
+}
+
+// 2 - Get Even
+
+// 3 - Excited Kittens
+
+// 4 - Fizz Buzz
+
+
+// 5 - Getting to Know You
+
+
+// 6 - Yell at the Ninja Turtles
+
+
+// 7 - Kristyn and Thom have their outfits ready for class
+
+
+// 8 - I loops through their closets
+
+
+// 9 - Project Euler Problem 1
+
