a93183460022bda676db4590002e0dc701274038
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-11 18:36:28 -0400

    Commit 4 - Fizz Buzz answered

--- answer.js
+++ answer.js
@@ -28,3 +28,21 @@ for(let i=0;i < 20;i++){
 }
 
 
+//"Commit 4 - Fizz Buzz answered"
+for(let i=1;i < 101;i++){
+    if( i%5 === 0 && i%3 === 0 ){
+      console.log("FizzBuzz");
+    }
+    else if(i%5 === 0 ){
+      console.log("Buzz");
+    }
+    else if(i%3 === 0){
+      console.log("Fizz");
+    }
+    else{
+      console.log(i);
+    }
+    }
+    
+
+
