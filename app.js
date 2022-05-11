4292e12496166e6da6759e1356c912ebf5d42fcc
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-11 19:20:23 -0400

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // 1. Easy Going
+
 for (let input = 1; input <= 20; input++) {
     console.log(input);
    }
 
 // 2. Get Even
+
 for (let i = 0; i <= 200; i +=2){
     console.log(i);
    }
 
 // 3. Excited Kitten
+
 const catArray=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 let catPhrase=["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
 let generate = Math.floor((Math.random(4)) * catPhrase.length);
@@ -37,3 +40,37 @@ for (let f = 1; f <= 100; f+=1)
    else {
        console.log(f)
    }
+
+// 5. Getting to Know You
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//Replace the string "Kenny" in the `kenny` array with "Gameboy"
+const firstName = kenny.shift();
+const newName = kenny.unshift("Gameboy");
+   console.log(kenny)
+
+//Jim Clark just had his birthday; he's now a year older
+jimClark[1] += 1;
+console.log(jimClark);
+
+//Change Ryan's hometown from "Denver" to "Gotham City".
+const oldCity = ryan.pop();
+const newCity = ryan.push("Gotham");
+   console.log(ryan)
+   
+//Reuben left Durham for Chicago
+const firstCity = reuben.pop();
+const secondCity = reuben.push("Chicago");
+   console.log(reuben)
+
+// Jim Haff can be in any of the 3 cities
+jimHaff.splice(2, 3, "Los Angeles", "New York", "Philly")  
+   console.log(jimHaff)
+
+jimHaff.splice(2, 1, );
+   console.log(jimHaff)
