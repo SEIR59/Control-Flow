f54acea17e41bfda065b2e300d74b11dee8ab649
yonatankumsa <yonatankumsa99@gmail.com>
2022-05-11 19:30:22 -0400

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // queetion 1
-for(i =1; i < 21;i++){
-    console.log(i);
-}
+//for(i =1; i < 21;i++){
+//    console.log(i);
+//}
 
+// question 2
+for(let i=0;i <201;i+=2){
+    console.log(i);
+    }
