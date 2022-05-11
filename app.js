f060247cc1bd2a646c1f130fb3ef6a903ecdd4c2
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-11 17:28:39 -0400

    Commit 1 - Easy Going answered

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+Homework - 05/11/2022
+Owner: Wendy Yu
+Date: 05/11/2022
+*/
+
+//1. Write a for loop that will log the numbers 1 through 20.
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
