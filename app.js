f7645a6ed811532cf9f3809870d6b1f9e6c3e935
Freightscape <jordan@freightscape.net>
2022-05-11 19:25:29 -0400

    Commit 2 - Get Even Answered

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-//Creating a for loop of 1 through 20
+// EASY GOING 1 - Creating a for loop of 1 through 20
 for (let i = 1; i <= 20; i++) {
-    console.log(i)
+    console.log(i);
 };
+// started this with a 0 instead of a 1 changed it and console logged in chrome, got the right answer
+// Note: Roberto Ramos helped with grasp of concepts on for loops prior to starting homework
+// loading via git
+
+
+// GET EVEN 2 - creating a loop that logs even numbers only up to 200
+for (let i = 2; i < 202; i+=2){
+    console.log(i);
+};
+// first tried by multiplying by "i*2", then by adding via "i+2", then went to google
+// my answer was close found the solution here: 
+
+// Stephen Van Delinder on Feb 15, 2015
+// You need to create a for loop that initializes the variable to 2, console.log the 
+// variable to the screen, and add two to the variable on each iteration. Said another way: 
+// starting at two, as long as i is less than 25, add two and log the new variable to the screen.
