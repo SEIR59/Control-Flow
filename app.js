baab54127f825aa2cc359ba82e9fda3ea23a1c2c
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-11 17:57:44 -0400

    Commit 3 - Excited Kittens - Needs reworked

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-// 1. Easy Going Solution
+// 1. Easy Going
 for (let i = 1; i <= 20; i++ ){
     console.log(i)
-} 
+}
+// 2. Excited Kitten
+for (let b = 1; b <= 20; b++){
+    console.log(`Love me, pet me! HSSSSSS!`)
+}
+let phrases = ["...human...why you taking pictures of me?..." , "...the catnip made me do it..." , "...why does the red dot always get away..."];
+let phrase = phrases[Math.floor(Math.random() * phrases.length)];
+for (let b = 1; b <= 20; b++){
+    if (b % 2 === 0){
+        console.log(`${phrase}`)
+    } else {
+        console.log(`Not an even number`)
+    }
+}
