af2911583f8bb03e23825ae68073088c4a67c4e6
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-11 17:45:44 -0400

    Commit 3.1  - Excited Kitchen 1 Answered

--- app.js
+++ app.js
@@ -12,8 +12,25 @@
 //1. Write a `for` loop that will log only the even numbers in 0 through 200.
 //>Hint: Think about the increment expression.
 
-for(let i = 0; i <=200; i+=2){
-    console.log(i)
+// for(let i = 0; i <=200; i+=2){
+//     console.log(i)
+// }
+
+//## Excited Kitten
+//1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+
+let i = 0
+
+while(i<=20){
+    console.log("Love me, pet me! HSSSSSS!")
+    i++;
 }
+
+
+//2. For every **even** number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+
+
+
+//>Hint: You will need to use Math.random()
 
 
