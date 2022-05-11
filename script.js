e2bc5a1109bf2d34a92c3186ca7a220dfb75c32c
andersons1218 <andersons1218@gmail.com>
2022-05-11 19:26:11 -0400

    Getting to Know You answered

--- script.js
+++ script.js
@@ -4,5 +4,43 @@
 // for (let i = 0; i <= 200; i += 2)
  // console.log(i)
 
-for ( let i = 0; i <= 20; i++)
-console.log("Love me, pet me! HSSSSSS!")
+// for ( let i = 0; i <= 20; i++)
+// console.log("Love me, pet me! HSSSSSS!")
+// Math.random
+
+// for (let i = 0; i <= 100; i++){
+// if (i % 3 === 0 && i % 5 ===0){
+// console.log("FizzBuzz")}
+
+// else if (i % 3 === 0){
+
+// console.log("Fizz")}
+
+// else if (i % 5 === 0){
+// console.log("Buzz")}
+
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = "Gameboy"
+console.log(kenny)
+
+jimClark[1] += 1
+console.log(jimClark)
+
+reuben.pop(2)
+console.log(reuben)
+reuben.push("Chicago")
+console.log(reuben)
+
+jimHaff.pop(2)
+console.log(reuben)
+jimHaff.push("Boston","New york","Florida")
+console.log(jimHaff)
+jimHaff.splice(2,1)
+console.log(jimHaff)
+
