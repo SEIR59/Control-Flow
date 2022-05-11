1d5d14053879d947d4772b2111622fdedf1ae4b7
andersons1218 <andersons1218@gmail.com>
2022-05-11 17:57:25 -0400

    Easy Going Answered

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let i = 0; i<= 200; i++)
+console.log([i])
+
+// for (let i = 0; i <= 200; i += 2)
+// console.log(i)
+
+// for ( let i = 0; i <= 20; i++)
+//console.log("Love me, pet me! HSSSSSS!")
