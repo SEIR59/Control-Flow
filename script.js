54da5512a7efef41b3c85c765ff8e7b95ba873c8
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-11 17:36:29 -0400

    Commit 1 - Easy Going answered

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// 1. Write a for loop that will log the numbers 1 through 20.
+
+const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for (let i = 0; i < myNum.length; i++){
+  console.log(myNum[i])
+}
