8e48117e7ad1abdecbb75f43eed89ec6dfff8bab
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-11 18:25:03 -0400

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ for (let b = 1; b <= 20; b++){
     console.log(`Love me, pet me! HSSSSSS!`)
 }
 let phrases = ["...human...why you taking pictures of me?..." , "...the catnip made me do it..." , "...why does the red dot always get away..."];
-let phrase = phrases[Math.floor(Math.random() * phrases.length)];
 for (let b = 1; b <= 20; b++){
+    let randomPhrase = Math.floor(Math.random() * phrases.length);
     if (b % 2 === 0){
-        console.log(`${phrase}`)
+        console.log(`${phrases[randomPhrase]}`)
     } else {
         console.log(`Not an even number`)
+    }
+}
+// Fizz Bizz
+for (let c = 1; c <= 100; i ++){
+    if (c / 3){
+        console.log(`Fizz`)
+    }
+    if else (c / 5) {
+        console.log(`Buzz`)
+    }
+    else (c / 3 && c / 5){
+        console.log(`FizzBuzz`)
     }
 }
