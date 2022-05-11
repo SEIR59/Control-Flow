dd98e47b1241772ed206175ba70cb943c61ff563
patrickbasham <PatrickTBasham@gmail.com>
2022-05-11 17:53:14 -0400

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ for ( let i = 1; i <= 20; i++) {
     console.log(i)
 }
 
-// 1. Get Even 
+// 1. Get Even // 0 is an even number. //
 for (let i = 0; i <= 200; i++) {
     if (i % 2 === 0) {
         console.log(i);
+    }
+}
+
+// 1. Excited Kitten
+const arrKittenSpeak = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+let randomNum;
+for (let i = 0; i < 20; i++){
+    console.log("Love me, pet me! HSSSSSS!");
+    if (i % 2 === 0) {
+        randomNum = Math.floor(Math.random() * 3);
+        console.log(arrKittenSpeak[randomNum]);
     }
 }
