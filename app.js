440ace171f6e9f667d334fce6ea7e297b38dadc6
AmandaFrisk <amanda.frisk33@gmail.com>
2022-05-11 17:21:39 -0600

    Easy Going answered

---
+++ app.js
@@ -0,0 +1,11 @@
+//## Easy Going
+//1. Write a for loop that will log the numbers 1 through 20.
+
+//<hr>
+//&#x1F534; The commit message should read: <br>
+//"Commit 1 - Easy Going answered"
+let i = 1;
+for (let i = 1; i< 21; i++)[
+    console.log([i])
+]
+
