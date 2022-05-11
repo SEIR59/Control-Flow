61262c0f04609d15b3d84dd3ae18dca5d1715a2f
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-11 14:46:43 -0700

    Commit 4 - Fizz Buzz answered

--- conditional_practice.js
+++ conditional_practice.js
@@ -18,3 +18,19 @@ for (let i = 0 ; i<20 ; i++){
       console.log(response[Math.floor(Math.random()*3)])
     }
   }
+
+  // Fizz Buzz
+for (let i = 1 ; i <101 ; i++){
+    solution = ''
+    if (i%3 ==0){
+      solution +="Fizz"
+    }
+    if(i%5 == 0){
+      solution+="Buzz"
+    }
+    if(solution.length == 0){
+      console.log(i)
+    }else{
+      console.log(solution)
+    }
+  }
