2cde1a2c0f1819325038b4dbcfa5b6e6fbc2a7bc
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-11 18:55:21 -0400

    Commit 5 - Getting to Know You answered

--- script.js
+++ script.js
@@ -63,55 +63,51 @@
 
 // 1. If a number is divisible by 3 log "Fizz" instead of the number.
 // 1. If a number is divisible by 5 log "Buzz" instead of the number.
-// 1. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number, "Fizz", or "Buzz".
+// // 1. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number, "Fizz", or "Buzz".
 
-// ## Getting to Know You
+// // ## Getting to Know You
 
-// Use the following arrays to answer the questions below (name, age, hometown):
+// // Use the following arrays to answer the questions below (name, age, hometown):
 
-// ```javascript
-const kenny = ["Kenny", 1000, "Austin"];
-const jimHaff = ["Jim H", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const jimClark = ["Jim C", 186, "LA"];
-const ryan = ["Ryan", 65, "Denver"];
-// ```
+// // ```javascript
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
+// // ```
 
-// 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Replace the string "Kenny" in the `kenny` array with "Gameboy".
+// // 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Replace the string "Kenny" in the `kenny` array with "Gameboy".
 
 kenny[0] = "Gameboy";
 
-// console.log(kenny);
+// // console.log(kenny);
 
-// 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code `187`--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+// // 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code `187`--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
 
 jimClark[1] = jimClark[1] + 1;
 
-// console.log(jimClark)
+// // console.log(jimClark)
 
-// 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+// // 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
 ryan[2] = "Gotham City";
 
-// console.log(ryan);
+// // console.log(ryan);
 
-// 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+// // 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
 
 reuben.pop();
 reuben.push("Chicago");
 
-// console.log(reuben);
+// // console.log(reuben);
 
 
-// 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but [see if you can do it in one line of code](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
+// // 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but [see if you can do it in one line of code](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
 
 jimHaff.pop();
 jimHaff.push("London", "Paris", "Los Angeles");
 
-console.log(jimHaff);
+// console.log(jimHaff);
 
 // 6. Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities. Whatever the _first_ of the 3 cities for Jim Haff is now, remove it from the array using [`.splice()`](https://www.google.com/search?q=mdn+splice&oq=mdn+splice&aqs=chrome.0.0j69i60j0l4.1078j0j7&sourceid=chrome&ie=UTF-8)
-
-
-
-
 
