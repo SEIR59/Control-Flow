ba1142915dbb147af6c35b5a6c6848753e588871
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-11 18:17:23 -0400

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ for (let input = 1; input <= 20; input++) {
 for (let i = 0; i <= 200; i +=2){
     console.log(i);
    }
+
+// 3. Excited Kitten
+const catArray=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+let catPhrase=["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+let generate = Math.floor((Math.random(4)) * catPhrase.length);
+for (let c = 0; c < catArray.length; c++){
+   if(catArray[c] % 2 === 0)
+       console.log(catPhrase[generate])
+    else{
+        console.log("Love me, pet me! HSSSSSS!")
+    }
+   }
+//its random every time i run it, but its not random every time it runs the loop
