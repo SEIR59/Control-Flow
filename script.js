b77c83b6d8002f7e59a492e62b959807a22a9f3a
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-11 17:52:52 -0400

    Commit 2 - Get Even answered

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
 // 1. Write a for loop that will log the numbers 1 through 20.
 
-const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-for (let i = 0; i < myNum.length; i++){
-  console.log(myNum[i])
+// for (let i = 0; i < myNum.length; i++){
+//   console.log(myNum[i])
+// }
+
+// ## Get Even
+// 1. Write a `for` loop that will log only the even numbers in 0 through 200.
+//    > Hint: Think about the increment expression.
+
+for (let i = 0; i <= 200; i+=2){
+    console.log(i);
 }
+
+
+
+
