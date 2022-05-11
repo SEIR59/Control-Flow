79c4a461659f894468f8e2fcca82b35669e6c023
yonatankumsa <yonatankumsa99@gmail.com>
2022-05-11 19:34:07 -0400

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -4,6 +4,23 @@
 //}
 
 // question 2
-for(let i=0;i <201;i+=2){
-    console.log(i);
-    }
+//for(let i=0;i <201;i+=2){
+//  console.log(i);
+// }
+
+// question 3
+let even = [
+  "...human...why you taking pictures of me?...",
+  "...the catnip made me do it...",
+  "...why does the red dot always get away...",
+];
+
+let all = ["Love me, pet me! HSSSSSS!"];
+
+for (let i = 1; i < 21; i++) {
+  console.log(all);
+
+  if (i % 2 === 0) {
+    console.log(even[Math.floor(Math.random() * even.length)]);
+  }
+}
