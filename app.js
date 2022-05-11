7b74a51c8ba05f202207f11e2104d17942d44918
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-11 18:34:02 -0400

    Commit 9 - Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -165,3 +165,22 @@ kristynsCloset.forEach((item) => {
 thomsCloset.forEach((closet) => {
     console.log(closet);
 })
+
+// ## Multiples of 3 and 5
+
+// _Yes, you might have tackled this earlier, but try it again (don't look back at your other code :eyes:)_
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the **sum** of all the multiples of 3 or 5 below 1000.
+let multiplesSum = 0;
+for (let i = 0; i < 1000; i++ ) {
+    if (i % 3 === 0 || i % 5 === 0){
+        multiplesSum += i
+    }
+}
+console.log(multiplesSum);
+
+// :clap: You just solved [Project Euler](https://projecteuler.net/problem=1) problem 1! :clap:
+
+// >Tip: Get used to thinking about how to solve problems _now_. Will help immensely with coding challenges, and get your coding brain muscles nice and flexed for the class. Make a habit of going to Project Euler and taking a crack at the problems
