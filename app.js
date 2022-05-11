076d5e933deb748c7ac65b77d1362723f2a03ce4
victortan <vtan8090@gmail.com>
2022-05-11 17:34:38 -0400

    Getting to Know You answered

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-for (let i = 1; i <+ 20 ; i++) {
+for (let i = 1; i < + 20; i++) {
     console.log(i);
 }
 
-for (let i = 0; i <= 200; i++){
-    if (i % 2 === 0){
-        console.log(i);
+for (let j = 0; j <= 200; j++) {
+    if (j % 2 === 0) {
+        console.log(j);
     }
 }
+
+let catArray = ["...human...why are you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+for (let k = 0; k < 20; k++) {
+    console.log("Love me, pet me! HSSSSSS");
+    if (k % 2 === 0) {
+        console.log(catArray[Math.floor(Math.random() * 2)])
+    }
+}
+
+
+const numArray = [];
+
+for (let l = 1; l <= 100; l++) {
+    if (l % 3 === 0 && l % 5 === 0) numArray.push("FizzBuzz");
+    else if (l % 3 === 0) numArray.push("Fizz");
+    else if (l % 5 === 0) numArray.push("Buzz");
+    else numArray.push(l);
+}
+
+console.log(numArray);
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = "Gameboy";
+jimClark[1]++;
+ryan[2] = "Gotham City";
+reuben.pop();
+reuben.push("Chicago";)
+jimHaff.pop()
+jimHaff.push("New York City", "San Francisco", "Guangzhou");
