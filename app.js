bd9af49c6dd954bcbbdf999d29a37abf94634745
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-05-11 18:17:04 -0400

    commit_3_Excited_Kitten_Answered

--- app.js
+++ app.js
@@ -9,3 +9,26 @@ for(let k = 1; k <= 20; k+=1){
 for(let i =0;i <=200; i+=2){
     console.log(i);
 }
+
+//commit 3 Excited Kitten
+
+
+let catPhrase = ["...the catnip made me do it...", "...why does the red dot always get away..." ]
+let generate = Math.floor((Math.random()) * catPhrase.length);
+let generate2 = Math.floor((Math.random()) * catPhrase.length);
+var catArray =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for (var q =0; q<catArray.length; q++){
+    if(catArray[q] % 2 ===0)
+    console.log(catPhrase[generate])
+else if(catArray[q] % 4===0){
+    console.log(catPhrase[generate2])
+}
+
+    
+else{
+    console.log("...human...why you taking pictures of me?...")
+}
+
+} 
+//its choshing a random phase every refresh, but not every log 
+
