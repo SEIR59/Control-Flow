d533e64885891fde56b692bb9cbfd114676b5291
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-11 16:51:53 -0400

    Commit 1 - Easy Going answered

---
+++ app.js
@@ -0,0 +1,11 @@
+//1
+//We fork so that everyone has permission to edit. We also fork that it shows up on our own reposity.
+
+// Easy Going
+// Write a for loop that will log the numbers 1 through 20.
+
+// for(let i = 1; i<=20; i++){
+//     console.log(i)
+// }
+
+
