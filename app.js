c90ba47c7f2f3c3a6c16b8069e317e7dc0759d7c
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-11 17:51:39 -0400

    Commit 3 - Excited Kittens answered & Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -4,7 +4,42 @@ Owner: Wendy Yu
 Date: 05/11/2022
 */
 
+/** Get Even */
 //1. Write a for loop that will log the numbers 1 through 20.
 for (let i = 1; i <= 20; i++) {
   console.log(i);
+}
+
+//2. Write a `for` loop that will log only the even numbers in 0 through 200.
+for (let i = 0; i <= 200; i += 2) {
+  console.log(i);
+}
+
+/** Excited Kitten  */
+for (let i = 1; i <= 20; i++) {
+  console.log("Love me, pet me! HSSSSSS!");
+
+  const meow = [
+    "...human...why you taking pictures of me?...",
+    "...the catnip made me do it...",
+    "...why does the red dot always get away...",
+  ];
+  //only even number log..
+  if (i % 2 === 0) {
+    let index = Math.floor(Math.random() * 3);
+    console.log(meow[index]);
+  }
+}
+
+/** Fizz Buzz */
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
