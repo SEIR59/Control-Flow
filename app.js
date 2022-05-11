35da7821e39310a97bd0d62585a2c34653ed5112
patrickbasham <PatrickTBasham@gmail.com>
2022-05-11 17:43:54 -0400

    Commit 2 - Get Even answered

---
+++ app.js
@@ -0,0 +1,11 @@
+// 1. Easy Going
+for ( let i = 1; i <= 20; i++) {
+    console.log(i)
+}
+
+// 1. Get Even 
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
