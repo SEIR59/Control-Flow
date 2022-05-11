ba5f7b968b412d45a0b78c12c81829bfb087f553
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-05-11 18:32:05 -0400

    commit_4_FizzBuzz_Answered

--- app.js
+++ app.js
@@ -32,3 +32,22 @@ else{
 } 
 //its choshing a random phase every refresh, but not every log 
 
+
+
+
+// Commit 4 Fizz Buzz
+
+for(let f = 1; f <= 200; f+=1)
+if(f %15===0){
+    console.log("FizzBuzz")
+}
+
+else if(f % 3 ===0){
+    console.log("Fizz")
+
+}else if(f % 5 ===0){
+    console.log("Buzz")
+}
+else{
+console.log(f)
+}
