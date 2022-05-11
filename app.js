7e482d6ead6b24017e2e5b851b4f2b5de085577e
patrickbasham <PatrickTBasham@gmail.com>
2022-05-11 18:01:46 -0400

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -21,3 +21,15 @@ for (let i = 0; i < 20; i++){
     }
 }
 
+// Fizz Buzz
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz " + i);
+    } else if (i % 3 === 0) {
+        console.log("Fizz " + i);
+    } else if ( i % 5 === 0) {
+        console.log("Buzz " + i);
+    } else {
+        console.log(i)
+    }
+}
