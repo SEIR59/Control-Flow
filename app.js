37bed86250578b9fe12cd56e9aceb51545ad1c78
patrickbasham <PatrickTBasham@gmail.com>
2022-05-11 17:54:11 -0400

    Fixed Headers

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// 1. Easy Going
+// Easy Going
 for ( let i = 1; i <= 20; i++) {
     console.log(i)
 }
 
-// 1. Get Even // 0 is an even number. //
+// Get Even // 0 is an even number. //
 for (let i = 0; i <= 200; i++) {
     if (i % 2 === 0) {
         console.log(i);
     }
 }
 
-// 1. Excited Kitten
+// Excited Kitten
 const arrKittenSpeak = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
 let randomNum;
 for (let i = 0; i < 20; i++){
@@ -20,3 +20,4 @@ for (let i = 0; i < 20; i++){
         console.log(arrKittenSpeak[randomNum]);
     }
 }
+
