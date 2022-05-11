bc1af32a03a407577f66e8faed300ee0058b6ba2
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-11 17:07:49 -0400

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -3,3 +3,12 @@
 for (let num = 1; num <= 20; num++){
     console.log(num);
 }
+
+// ## Get Even
+// 1. Write a `for` loop that will log only the even numbers in 0 through 200.
+// >Hint: Think about the increment expression.
+for (let num = 0; num <= 200; num++) {
+    if (num % 2 === 0) {
+        console.log(num)
+    }
+}
