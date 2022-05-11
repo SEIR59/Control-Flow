a51cd1509783e41ad4678bb14dd7744b77962fc2
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-11 19:46:09 -0400

    Commit 8 - I loops through their closets.

--- script.js
+++ script.js
@@ -17,30 +17,30 @@
 
 // ## Excited Kitten
 
-// 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+// // 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
 
-for (i = 0; i < 20; i++){
-  console.log("Love me, pet me! HSSSSSS!")
-}
-
-
-// 2. For every **even** number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+// for (i = 0; i < 20; i++){
+//   console.log("Love me, pet me! HSSSSSS!")
+// }
 
 
+// // 2. For every **even** number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 
-for (i = 0; i <= 20; i++){
-  console.log("Love me, pet me! HSSSSSS!")
-  if (i%2 ===0){
-  r = Math.floor(Math.random()*3);
-  if (r === 0) {
-    console.log("...human...why you taking pictures of me?...")
-  } else if (r === 1) {
-    console.log("...the catnip made me do it...")
-  } else if (r === 0) {
-    console.log("...why does the red dot always get away...")
-  }
-}
-} 
+
+
+// for (i = 0; i <= 20; i++){
+//   console.log("Love me, pet me! HSSSSSS!")
+//   if (i%2 ===0){
+//   r = Math.floor(Math.random()*3);
+//   if (r === 0) {
+//     console.log("...human...why you taking pictures of me?...")
+//   } else if (r === 1) {
+//     console.log("...the catnip made me do it...")
+//   } else if (r === 0) {
+//     console.log("...why does the red dot always get away...")
+//   }
+// }
+// } 
 
 
 
@@ -202,4 +202,6 @@ for ( i = 0; i < kristynsCloset.length; i++){
 
 // 2. Thom wants to do inventory on his closet. Using **bracket notation**, log the **arrays** (not the elements in the arrays) containing all of Thom's shirts, pants, and accessories.
 
-
+for ( i = 0; i < thomsCloset.length; i++){
+  console.log(thomsCloset[i]);
+}
