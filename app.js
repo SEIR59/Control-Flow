acb5a2ec8a603aeff080a7dc0dc369a64d76ebf7
earlhalasan <earl.halasan@gmail.com>
2022-05-12 02:20:10 -0400

    Commit 9 - Project Euler Problem 1 answered... kind of

--- app.js
+++ app.js
@@ -113,3 +113,16 @@ thomsCloset[1] = "Pants"
 thomsCloset[2] = "Accessories"
 
 console.log(thomsCloset[0], thomsCloset[1], thomsCloset[2])
+
+// 9. I had to look his one up
+console.log("MULTIPLES OF 3 AND 5")
+function sumOfMultiple(limit) {
+    var i, sum = 0;
+    for (i = 3; i < limit; i += 1) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+console.log(sumOfMultiple(1000));
