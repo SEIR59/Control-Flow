0b85c22bf16bdf2189c484e554a21af546302a04
Freightscape <jordan@freightscape.net>
2022-05-12 08:53:32 -0400

    Commit 5 - Getting to Know You Answered

--- app.js
+++ app.js
@@ -56,3 +56,26 @@ for (let i = 0; i < 100; i++) {
 // I know that there is a Else If statement that'll follow the first "if" that should account for the Divisible by 5 indecies
 // I understand the concept but dont know how to articulate it in JS
 // ...moving on
+
+
+// GETTING TO KNOW YOU - 
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Replace the string "Kenny" in the kenny array with "Gameboy".
+const newKenny = kenny.replace("Kenny", "Gameboy");
+// found this article online but still not getting the correct console.log
+// https://www.w3schools.com/jsref/jsref_replace.asp
+// 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code 187--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+const newJimClark = jimClark.replace(186, ([1] ++1);
+// 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+const newRyan = ryan.replace("Denver", "Gotham City");
+// 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+reuben.pop
+reuben.push("Chicago")
+// 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+const newJimHaff = jimHaff.replace("All cities", ["New York", "Singapore", "Cumberland"]);
+// 6. passed on Bonus because of time constraints (t-Minus 8 minutes to start of class)
