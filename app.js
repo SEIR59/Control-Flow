4fe752a1834bb5a22d3689742b46d5c855722dac
mileslathan <mlathan.cn@gmail.com>
2022-05-11 21:33:47 -0400

    Get Even answered

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-//EASY GOING
+// //EASY GOING
 
-for (let i = 1; i <= 20; i++) {
-    console.log(i)
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i)
+// }
+
+// Get Even
+
+for (let i = 1; i <= 200; i++) {
+    i += 1
+    console.log(i);
 }
