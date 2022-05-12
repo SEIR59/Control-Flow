4ad37a0019691db429ba5893fd10766ddfa0c1d5
andersons1218 <andersons1218@gmail.com>
2022-05-11 21:23:49 -0400

    Commit 8 - I loops through their closets.

--- apps.js
+++ apps.js
@@ -51,42 +51,45 @@
 // }
 
 const kristynsCloset = [
-    "left shoe",
+   "left shoe",
     "cowboy boots",
-    "right sock",
-    "GA hoodie",
-    "green pants",
-    "yellow knit hat",
+   "right sock",
+     "GA hoodie",
+   "green pants",
+     "yellow knit hat",
     "marshmallow peeps"
-  ];
+   ];
   
-  // Thom's closet is more complicated. Check out this nested data structure!!
-  const thomsCloset = [
-    [
-      // These are Thom's shirts
-      "grey button-up",
-      "dark grey button-up",
-      "light blue button-up",
-      "blue button-up",
-    ],[
-      // These are Thom's pants
-      "grey jeans",
-      "jeans",
-      "PJs"
-    ],[
-      // Thom's accessories
-      "wool mittens",
-      "wool scarf",
-      "raybans"
-    ]
-  ]
-const w = kristynsCloset.slice(0,1)
-console.log(w)
+  // // Thom's closet is more complicated. Check out this nested data structure!!
+  // // const thomsCloset = [
+  //   // [
+  //     // These are Thom's shirts
+  //     "grey button-up",
+  //     "dark grey button-up",
+  //     "light blue button-up",
+  //     "blue button-up",
+  //   // ],[
+  //     // These are Thom's pants
+  //     "grey jeans",
+  //     "jeans",
+  //     "PJs"
+  //   // ],[
+  //     // Thom's accessories
+  //     "wool mittens",
+  //     "wool scarf",
+  //     "raybans"
+    // ]
+  // ]
+// const w = kristynsCloset.slice(0,1)
+// console.log(w)
 
-thomsCloset.push(w)
-console.log(thomsCloset)
+// thomsCloset.push(w)
+// console.log(thomsCloset)
 
-kristynsCloset[0,1,2]
-console.log(`kristyn is wearing a ${kristynsCloset[0]}, ${kristynsCloset[1]}, and a ${kristynsCloset[2]}`)
+// kristynsCloset[0,1,2]
+// console.log(`kristyn is wearing a ${kristynsCloset[0]}, ${kristynsCloset[1]}, and a ${kristynsCloset[2]}`)
 
+
+console. log ("WHIRR: Now washing " + kristynsCloset[Math.floor (Math.random
+  ())*kristynsCloset.length])
 
