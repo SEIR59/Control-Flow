baae144c45a1433ba7e865d4c236bebff969401c
mileslathan <mlathan.cn@gmail.com>
2022-05-11 23:39:28 -0400

    Commit 9 - Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -104,15 +104,17 @@ const kristynsCloset = [
 //   console.log("Kristyn is wearing " + kristynsCloset[2] + ", " + kristynsCloset[3] + " and " + kristynsCloset[0] + ".");
 
 // I loops through their closets
- for (let i = 0; i < kristynsCloset.length; i++) {
-     console.log("WHIRR: Now washing " + kristynsCloset[i]);
- }
+//  for (let i = 0; i < kristynsCloset.length; i++) {
+//      console.log("WHIRR: Now washing " + kristynsCloset[i]);
+//  }
  
- console.log(thomsCloset[0].length + thomsCloset[1].length + thomsCloset[2].length);
+//  console.log(thomsCloset[0].length + thomsCloset[1].length + thomsCloset[2].length);
 
-// Multiples of 3 and 5
-// for (let i = 0; i <= 100; i++) {
-//     if (i % 3 === 0 || i % 5 === 0) {
-//         console.log(i)
-//     }
-// }
+ // Multiples of 3 and 5
+ sum = 0
+ for (let i = 0; i <= 1000; i++) {
+     if (i % 3 === 0 || i % 5 === 0) {
+         sum += i;
+     }
+ }
+ console.log(sum);
