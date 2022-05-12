56ebda3fb1f550bfa6082599f7b32bf76e49fe90
heysungj <linjing0525@gmail.com>
2022-05-11 22:43:59 -0400

    Commit 6 - Yell at the Ninja Turtles answered

--- app.js
+++ app.js
@@ -79,3 +79,23 @@ console.log(jimHaff);
 
 // 6. Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities.  Whatever the _first_ of the 3 cities for Jim Haff is now, remove it from the array using [`.splice()`](https://www.google.com/search?q=mdn+splice&oq=mdn+splice&aqs=chrome.0.0j69i60j0l4.1078j0j7&sourceid=chrome&ie=UTF-8)
 jimHaff.splice(2, 1);
+
+// ## Yell at the Ninja Turtles
+// 1. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+// 2. Use a `for loop` to call `.toUpperCase()` on each of them and print out the result.
+for (let i = 0; i < ninjaTurtles.length; i++) {
+  console.log(ninjaTurtles[i].toUpperCase());
+}
+// 3. Bonus: Modify the answer you just wrote. Instead of all letters being uppercase, make the letters alternate back and forth between uppercase and lowercase.
+for (let i = 0; i < ninjaTurtles.length; i++) {
+  let name = [];
+  for (let j = 0; j < ninjaTurtles[i].length; j++) {
+    if (j % 2 === 0) {
+      name.push(ninjaTurtles[i][j].toUpperCase());
+    } else {
+      name.push(ninjaTurtles[i][j].toLowerCase());
+    }
+  }
+  console.log(name.join(""));
+}
