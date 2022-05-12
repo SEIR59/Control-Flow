911b4b2e0690b798c87374ed9689acbdbb387e15
hnhtran <tran_eetas@yahoo.com>
2022-05-12 18:13:37 +1000

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -47,3 +47,25 @@ for(let i = 1; i < 101; i++) {
         console.log(i + " Buzz");
     } 
 }
+
+//#####Getting to Know You######//
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+// Jim Clark decides that Kenny can't be named "Kenny" anymore. Replace the string "Kenny" in the kenny array with "Gameboy".
+kenny[0] = "Gameboy";
+// Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code 187--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+jimClark[1] += 1;
+//Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+ryan[2] = "Gotham City";
+//Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+reuben.pop();
+reuben.push("Chicago");
+//Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+jimHaff.pop();
+jimHaff.push("Chicago", "Ho Chi Minh", "New York");
+//Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim Haff is now, remove it from the array using .splice()
+jimHaff.splice(4, 1);
+console.log(jimHaff);
