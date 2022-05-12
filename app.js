d299ad19e1f2089791a1fddc86f72fdcfbb32de8
patwyzlic <patrickwyzlic@gmail.com>
2022-05-11 22:32:32 -0400

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ for(let i = 1; i <= 20; i++){
 for(let i = 0; i <= 200; i+=2){
     console.log(i);
 }
+
+//Log text 20 times, even numbers produce another text at random points, Excited Kitten
+let a = 0;
+let catSpeaks = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+while(a < 20){
+    let randomText = Math.floor(Math.random() * 3);
+    console.log("Love me, pet me! HSSSSSS!");
+    if(a % 2 == 0){
+        console.log(catSpeaks[randomText]);
+    }
+    a++;
+
+}
