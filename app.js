3da37a0a6f8e4abce18ae4bd7eab241e2ef8a3df
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-05-11 20:21:10 -0400

    Commit 9 - Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -136,5 +136,11 @@ for (let i = 0; i < kristynsCloset.length; i++) {
 
 console.log(thomsCloset)
 
-// 9 - Project Euler Problem 1
-
+// 9 - Project Euler Problem 1]
+let sum = 0
+for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        sum = sum + i
+    }
+}
+console.log(sum)
