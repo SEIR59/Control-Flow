9fa8c7afb3b834930e97748f031a8c22d65fe71c
idbic <dbicga@gmail.com>
2022-05-12 07:26:24 -0500

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //1. Easy Going
 let i = 0;
 
-for(let i = 1; i <= 20; i++) {
+//for(let i = 1; i <= 20; i++) {
+ //   console.log(i)
+//}
+
+
+//2. Get even 
+for(let i = 0; i <= 200; i += 2){
     console.log(i)
 }
-console.log(i)
+
+
+
+//3. Excited Kitted
+//for(i = 0; i < 20; i++) {
+  //  console.log("Love me, pet me! HSSSSSS!" )
+//}
+//console.log(i)
