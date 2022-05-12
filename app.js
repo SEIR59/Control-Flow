68a7dd1846c7595f7eba31b4b8bb5e526ecc8540
mileslathan <mlathan.cn@gmail.com>
2022-05-11 22:47:14 -0400

    Getting to Know You answered.

--- app.js
+++ app.js
@@ -21,14 +21,38 @@
 // }
 
 // Fizz Buzz
- for (let i = 0; i <= 100; i++) {
-     if (i % 3 === 0) {
-         console.log("Fizz")
-     } else if (i % 3 === 0 && i % 5 === 0) {
-         console.log("FizzBuzz")
-     } else if (i % 5 === 0) {
-         console.log("Buzz")
-     } else {
-        console.log(i)
-     }
- }
+//  for (let i = 0; i <= 100; i++) {
+//      if (i % 3 === 0) {
+//          console.log("Fizz")
+//      } else if (i % 3 === 0 && i % 5 === 0) {
+//          console.log("FizzBuzz")
+//      } else if (i % 5 === 0) {
+//          console.log("Buzz")
+//      } else {
+//         console.log(i)
+//      }
+//  }
+
+// Getting to Know You
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = "Gameboy"
+console.log(kenny);
+
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark[1]);
+
+ryan[2] = "Gotham City"
+
+reuben.pop();
+console.log(reuben);
+reuben.push("Chicago");
+console.log(reuben);
+
+jimHaff.pop();
+jimHaff.push("Singapore", "Hong Kong", "Shanghai");
+console.log(jimHaff);
