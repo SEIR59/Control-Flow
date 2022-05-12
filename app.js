196e2cfb62cfff19c6298418ce8ab9cd7e058251
poojad2101 <davepooja2101@gmail.com>
2022-05-12 12:01:22 +0530

    Excited Kittens answered

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+1. easy going
 // for (i = 0; i <= 20; i++)
-console.log(i); //
+// console.log(i);
+
+2. even answered
+//for(i = 0; i<=200; i +=2)
+// Console.log(i)
+
+3. excited kitten
+for (let a = 1; a <= 20; a++){
+    console.log(`Love me, pet me! HSSSSS!`)
+}
+let phrase = ["...human...why you taking pictures of me?..." , "...the catnip made me do it..."]
+let randomPhrase = (Math.floor(Math.random) * phrases.length);
+for (let a = 1; a <=20; a++){
+    if (a % 2 === 0){
+        console.log(`${phrases[Math.floor(Math.random) * phrase.length)]}`)
+    } else {
+        console.log(`Not an even number`)
+    }
+}
+
+
 
 
 
