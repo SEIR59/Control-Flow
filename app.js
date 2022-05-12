c2e2fb8774619aa65d98be0e69e98697bbbbc2f0
hnhtran <tran_eetas@yahoo.com>
2022-05-12 16:57:01 +1000

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ for(let i = 0; i <= 200; i++) {
     }
 }
 console.log(a2);
+
+//#####Excited Kitten######//
+//Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+for(let i = 1; i < 21; i++) {
+    let msg = " Love me, pet me! HSSSSSS!";
+    console.log(i + msg);
+}
+
+//For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+let a3 = ["...human...why you taking pictures of me?...", "..the catnip made me do it...", "...why does the red dot always get away..."];
+for(let i = 1; i < 21; i++) {
+    if (i % 2 == 0) {
+        let j = Math.floor((Math.random() * 3));
+        let rdmMsg = a3[j];
+        console.log(i + rdmMsg);
+    }
+}
