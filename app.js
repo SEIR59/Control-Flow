fd244af468fc0df6ec6ffed459612c86f57bba82
mileslathan <mlathan.cn@gmail.com>
2022-05-11 22:55:39 -0400

    Yell at the Ninja Turtles answered.

--- app.js
+++ app.js
@@ -34,25 +34,31 @@
 //  }
 
 // Getting to Know You
-const kenny = ["Kenny", 1000, "Austin"];
-const jimHaff = ["Jim H", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const jimClark = ["Jim C", 186, "LA"];
-const ryan = ["Ryan", 65, "Denver"];
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
 
-kenny[0] = "Gameboy"
-console.log(kenny);
+// kenny[0] = "Gameboy"
+// console.log(kenny);
 
-jimClark[1] = jimClark[1] + 1;
-console.log(jimClark[1]);
+// jimClark[1] = jimClark[1] + 1;
+// console.log(jimClark[1]);
 
-ryan[2] = "Gotham City"
+// ryan[2] = "Gotham City"
 
-reuben.pop();
-console.log(reuben);
-reuben.push("Chicago");
-console.log(reuben);
+// reuben.pop();
+// console.log(reuben);
+// reuben.push("Chicago");
+// console.log(reuben);
 
-jimHaff.pop();
-jimHaff.push("Singapore", "Hong Kong", "Shanghai");
-console.log(jimHaff);
+// jimHaff.pop();
+// jimHaff.push("Singapore", "Hong Kong", "Shanghai");
+// console.log(jimHaff);
+
+// Yell at the Ninja Turtles
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+for (let i = 0; i < ninjaTurtles.length; i++) {
+    console.log(ninjaTurtles[i].toUpperCase());
+}
