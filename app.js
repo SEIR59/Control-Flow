0667ebc9f0ad323af55b33818e2a96bf0ab76bcf
patwyzlic <patrickwyzlic@gmail.com>
2022-05-11 22:07:22 -0400

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 
-//Count 1 - 20 in console
+//Count 1 - 20 in console, Easy Going
 for(let i = 1; i <= 20; i++){
     console.log(i);
 }
 
+//Even numbers 0-200, Get Even
+for(let i = 0; i <= 200; i+=2){
+    console.log(i);
+}
