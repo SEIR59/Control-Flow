4bad7acab14ab156a70ac50227fad2760adcad38
earlhalasan <earl.halasan@gmail.com>
2022-05-11 23:33:26 -0400

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -25,3 +25,18 @@ for (let c = 1; c <= 20; c++) {
     }
     else console.log("Love me, pet me! HSSSSSS!")
  }
+
+ // 4.
+ console.log("Fizz Buzz")
+ for (let i = 1; i < 101; i++) {
+     if (i % 3 === 0) {
+         console.log("Fizz");
+     }
+     else if (i % 5 === 0) {
+         console.log("Buzz")
+     }
+     else if (i % 15 === 0) {
+         console.log("FizzBuzz")
+     }
+     else console.log(i);
+ }
