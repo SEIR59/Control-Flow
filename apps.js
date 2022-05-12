6257a1671731a15c925af93d10bc0dd1020601ea
“GissellValdez” <Gissellvaldez@gmail.com>
2022-05-11 21:14:04 -0400

    updated answers to questions 1 and 2

--- apps.js
+++ apps.js
@@ -1,3 +1,12 @@
 // Easy going //
 
-for (let i = 0; i < 21; i++ )
+for (let i = 1; i < 20; i++) {
+    console.log(i)
+}
+	
+    //  Get Even//
+
+	for (let i = 0; i < 200; i++) {
+        if (i % 2 === 0) 
+        console.log(i)
+    }
