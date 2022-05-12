01eb6ae9705f39020ab15baf1af3f03b24d4866c
mkhan42 <michallekhan9@gmail.com>
2022-05-11 23:54:18 -0400

    Commit 7 - Kristyn and Thom have their outfits ready for class - array practice

--- app.js
+++ app.js
@@ -163,7 +163,6 @@ for (let i = 0; i < ninjaTurtles.length; i++) {
 }
 
 
-
 /* Return of the Closets
 
 Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript. Use this data to answer the following questions.
@@ -200,16 +199,74 @@ const thomsCloset = [
   ]
 ];
 ```
-
-Alien Attire
-1. Remove Kristyn's shoe from the array and save it to the variable `kristynsShoe`. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
-
-Dress 'em Up
-1. Let's create some outfits for both Kristyn and Thom. Select 3 elements from `kristynsCloset` and inject them into a sentence that reads, for example, "Kristyn is wearing green pants, a right sock, and marshmallow peeps" so that "green pants", "right sock", and "marshmallow peeps" are dynamically injected into the string. Do the same thing for Thom using `thomsCloset`. (Here's a suggestion that's not required: If you really want to challenge yourself, see if you can make the selections randomly from each of their arrays.)
-
 The commit message should read: <br>
 "Commit 7 - Kristyn and Thom have their outfits ready for class - array practice"
 */
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+//Alien Attire
+//1. Remove Kristyn's shoe from the array and save it to the variable `kristynsShoe`. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
+
+let kristynsShoe = kristynsCloset.shift();
+console.log(kristynsCloset);
+thomsCloset[2].push(kristynsShoe);
+console.log(thomsCloset);
+
+
+//Dress 'em Up
+//1. Let's create some outfits for both Kristyn and Thom. Select 3 elements from `kristynsCloset` and inject them into a sentence that reads, for example, "Kristyn is wearing green pants, a right sock, and marshmallow peeps" so that "green pants", "right sock", and "marshmallow peeps" are dynamically injected into the string. Do the same thing for Thom using `thomsCloset`. (Here's a suggestion that's not required: If you really want to challenge yourself, see if you can make the selections randomly from each of their arrays.)
+
+let testArrayK = [];
+while (testArrayK.length < 3) {
+    let tempValK = Math.floor(Math.random() * kristynsCloset.length)
+    if (!testArrayK.includes(tempValK)){
+        testArrayK.push(tempValK);
+    }
+}
+
+console.log(`Kristyn is wearing ${kristynsCloset[testArrayK[0]]}, ${kristynsCloset[testArrayK[1]]}, and ${kristynsCloset[testArrayK[2]]}`)
+
+let testArrayT = [];
+while (testArrayT.length < 3) {
+    let tempValT = Math.floor(Math.random() * thomsCloset.length)
+    if (!testArrayT.includes(tempValT)){
+        testArrayT.push(tempValT);
+    }
+}
+
+console.log(`Thom is wearing ${thomsCloset[testArrayT[0]]}, ${thomsCloset[testArrayT[1]]}, and ${thomsCloset[testArrayT[2]]}`)
+
+
 
 /* Dirty Laundry
 Continue looking at the closet arrays:
