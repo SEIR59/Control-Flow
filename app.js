6b86c9eb5e9e957d9742d4c7d6c2e0cd3c543786
earlhalasan <earl.halasan@gmail.com>
2022-05-11 22:29:18 -0400

    Commit 1 - Easy Going answered

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-console.log("hello")
+// 1.
+console.log("Easy Going")
+for (let i = 1; i < 21; i++) {
+    console.log(i);
+}
 
-for (let i = 1; i < 201; i++) {
-    console.log(i)
+// 2.
+console.log("Get Even")
+for (let i = 0; i < 201; i += 2) {
+    console.log(i);
 }
