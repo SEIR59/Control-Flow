d462e9b1804293efe513ebc1f156148316e3cda8
annakoanna <anna.231991k@gmail.com>
2022-05-12 00:08:57 -0700

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -16,3 +16,17 @@ for(let i = 0; i < 20; i++){
 
     }
 }
+//4
+for(let i = 1; i <= 100; i++) {
+    
+    if( i % 3 === 0 && i % 5 !== 0){
+        console.log("Fizz");
+    } else if(i % 5 === 0 && i % 3 !== 0){
+        console.log("Buzz");
+    } else if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz")
+    } else {
+      console.log(i);
+    }
+
+}
