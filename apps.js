98289d218d264101ae8c104839cd1c8c919aadfc
“GissellValdez” <Gissellvaldez@gmail.com>
2022-05-11 22:19:24 -0400

    lined up answers to questions one and two

--- apps.js
+++ apps.js
@@ -1,12 +1,29 @@
 // Easy going //
 
-for (let i = 1; i < 20; i++) {
+for (let i =1; i <= 20; i++) {
     console.log(i)
 }
 	
-    //  Get Even//
+// Get Even //
 
-	for (let i = 0; i < 200; i++) {
-        if (i % 2 === 0) 
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 === 0) 
         console.log(i)
-    }
+ }
+
+// Excited Kitten //
+
+  // 1 //
+ for (let i = 0; i <= 20; i++) {
+	console.log('Love me, pet me! HSSSSSS!')
+ }
+ //2//
+
+ let catSays = ["...human...why you taking pictures of me?...", "...the catnip made me do it...","...why does the red dot always get away..."]
+ for (let i = 0; i <= 20; i++) {
+    console.log(i) 
+    console.log('Love me, pet me! HSSSSSS!')
+    if(i % 2 === 0)
+    console.log(catSays[(Math.floor(Math.random() * 3))])
+ }
+// 3 //
