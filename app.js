2f668f9c7d5dc79f49e25a2059ac7d39ee67b62b
NSTaft <taftnathan13@gmail.com>
2022-05-11 19:20:05 -0500

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ for (let i = 1; i < 21; i++) {
 Get Even
 
 #1
-*/
+
 for (let i = 0; i < 201; i += 2) {
      console.log(i);
 }
+*/
+
+// Excited Kitten
+
+// #1
+
+for (let i = 0; i < 20; i++) {
+    console.log("Love me, pet me! HSSSSSS!")
+// #2
+const myArr = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+const random = Math.floor(Math.random() * myArr.length);
+    if (i %2 === 0) {
+        console.log(myArr[random])}
+    }
