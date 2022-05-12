ac8d5580b3410cd40563b69306e8493bea3e04eb
poojad2101 <davepooja2101@gmail.com>
2022-05-12 12:18:59 +0530

    Fizz Buzz answered

--- app.js
+++ app.js
@@ -7,18 +7,28 @@
 // Console.log(i)
 
 3. excited kitten
-for (let a = 1; a <= 20; a++){
-    console.log(`Love me, pet me! HSSSSS!`)
-}
-let phrase = ["...human...why you taking pictures of me?..." , "...the catnip made me do it..."]
-let randomPhrase = (Math.floor(Math.random) * phrases.length);
-for (let a = 1; a <=20; a++){
-    if (a % 2 === 0){
-        console.log(`${phrases[Math.floor(Math.random) * phrase.length)]}`)
-    } else {
-        console.log(`Not an even number`)
+//for (let a = 1; a <= 20; a++){
+  //  console.log(`Love me, pet me! HSSSSS!`)
+//}
+//let phrase = ["...human...why you taking pictures of me?..." , "...the catnip made me do it..."]
+//let randomPhrase = (Math.floor(Math.random) * phrases.length);
+//for (let a = 1; a <=20; a++){
+    //if (a % 2 === 0){
+        //console.log(`${phrases[Math.floor(Math.random) * phrase.length)]}`)
+    //} else {
+        //console.log(`Not an even number`)
+    //}
+//}
+
+4. fizz buzz
+for (i = 1; i <= 100; i++){
+    if(i % 3 === 0) {
+        console.log("Fizz")
+    } else if (i % 5 === 0){
+        console.log("Buzz")
+    } else if (i % 3, 5 === 0){
+        console.log("FizzBuzz")}
     }
-}
 
 
 
