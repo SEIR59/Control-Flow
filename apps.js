036ac14da6622ba2c592826477ba05a2a0fda0b7
“GissellValdez” <Gissellvaldez@gmail.com>
2022-05-11 23:11:05 -0400

    commit 4 fizz buzz answered

--- apps.js
+++ apps.js
@@ -26,3 +26,16 @@ for (let i = 0; i <= 200; i++) {
     if(i % 2 === 0)
     console.log(catSays[(Math.floor(Math.random() * 3))])
  }
+// Fizz Buzz//
+
+for (let number = 1; number <= 15; number++) {
+	if (number % 3 == 0 && number % 5 == 0) {
+		console.log('FizzBuzz')
+	} else if (number % 3 == 0) {
+		console.log('Fizz')
+	} else if (number % 5 == 0) {
+		console.log('Buzz')
+	} else {
+		console.log(number)
+	}
+}
