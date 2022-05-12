52cc3994165f249688660a440bac033cdc9502b2
hnhtran <tran_eetas@yahoo.com>
2022-05-12 19:42:15 +1000

    Commit 9 - Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -138,3 +138,12 @@ for(let i = 0; i < thomsCloset.length; i++) {
 //Yes, you might have tackled this earlier, but try it again (don't look back at your other code 👀)
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
+let total = 0;
+for(let i = 0; i < 1000; i++) {
+    if (i % 3 == 0) {
+        total = total + i;
+    } else if (i % 5 == 0) {
+        total = total + i;
+    }
+}
+console.log(total);
