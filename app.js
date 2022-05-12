d8c0441cca4be8a6a1531235377f62898d447d6a
mkhan42 <michallekhan9@gmail.com>
2022-05-12 00:44:49 -0400

    Commit 9 - Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -311,6 +311,14 @@ The commit message should read:
 "Commit 9 - Project Euler Problem 1 answered"
 */
 
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+    if(i % 3 === 0 && i % 5 === 0)
+        sum += i;
+}
+
+console.log(sum);
+
 
 /* Hungry for more?
 
