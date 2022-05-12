f173d3943df7649ce4f3edc67c0b2672ba113edd
hnhtran <tran_eetas@yahoo.com>
2022-05-12 16:33:00 +1000

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-//Easy Going
-
+//#####Easy Going######//
 //Write a for loop that will log the numbers 1 through 20.
 const a1 = [];
 for(let i = 1; i < 21; i++) {
     a1.push(i);
 }
 console.log(a1);
+
+//#####Get Even######//
+//Write a for loop that will log only the even numbers in 0 through 200.
+const a2 = [];
+for(let i = 0; i <= 200; i++) {
+    if (i % 2 == 0) {
+        a2.push(i);
+    }
+}
+console.log(a2);
