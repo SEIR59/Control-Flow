fdf2b4d0e679142032629414574465918b92ea4d
AmandaFrisk <amanda.frisk33@gmail.com>
2022-05-11 20:14:01 -0600

    Excited Kittens answered

--- app.js
+++ app.js
@@ -8,9 +8,24 @@
  //## Get Even
 //1. Write a `for` loop that will log only the even numbers in 0 through 200.
    //>Hint: Think about the increment expression.
-   let i = 0;
-   for (i = 0; i < 200; i++) { 
-       if (i % 2 === 0) {
-           console.log(i);
-       }
-   }
+   //let i = 0;
+   //for (i = 0; i < 200; i++) { 
+     //  if (i % 2 === 0) {
+       //    console.log(i);
+       //}
+   //}
+  // ## Excited Kitten
+//1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+//let sentence = "Love me, pet me! HSSSSSS!"
+//for (let i = 0; i < 20; i++) {
+//console.log("Love me, pet me! HSSSSS!");
+    //}
+
+    // I'm not sure if this worked effectively. The line in the console had a 20 next to let but it didnt log 20 times. I wasn't sure if this stands for 2 times?
+//2. For every **even** number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0)
+   {console.log(math.random() , "...human...why you taking pictures of me?..." || "...the catnip made me do it..." || "...why does the red dot always get away..." );
+    }
+}
+//I wasn't sure what to do but this was my best guess.
