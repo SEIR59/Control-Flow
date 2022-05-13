205a83711f5ebf3702290d25f3db1fa819a90da0
Freightscape <jordan@freightscape.net>
2022-05-12 20:05:52 -0400

    Commit 5 - Getting to Know You answered (UPDATED)

--- app.js
+++ app.js
@@ -40,22 +40,25 @@ for (let i = 0; i < 20; i++) {
 
 
 // FIZ BUZZ logging all numbers 1-100: A. If divisible by 3 log "Fizz", B. If divisible by 5 log "Buzz", C. if divisbile by both log "FizzBuzz"
-let myString = ["Fizz", "Buzz", "FizzBuzz"];
-for (let i = 0; i < 100; i++) {
-    if (i % 3 === 0) {
-        console.log(myString["#'s divisible by 3"])
-    }
-    else if (i % 5 === 0) {
-        console.log(myString["#'s divisible by 5"])
-    }
-    else if (i % 3 && 5 === 0){
-        console.log(mystring["#'s divisible by 3 && 5, 3, or 5"])
+function myString (parameter) {
+    for (let i = 0; i < parameter; i++) {
+        if (i % 15 === 0) {
+            console.log("FizBuzz")
+        }
+        else if (i % 5 === 0) {
+            console.log("Buzz")
+        }
+        else if (i % 3 === 0) {
+            console.log("Fizz")
+        }
     }
 }
+console.log(myString(100));
 // To start, running same script as before but for ODD #'s
 // I know that there is a Else If statement that'll follow the first "if" that should account for the Divisible by 5 indecies
 // I understand the concept but dont know how to articulate it in JS
 // ...moving on
+// finsihing up got the answer right
 
 
 // GETTING TO KNOW YOU - 
@@ -66,18 +69,28 @@ const jimClark = ["Jim C", 186, "LA"];
 const ryan = ["Ryan", 65, "Denver"];
 
 // 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Replace the string "Kenny" in the kenny array with "Gameboy".
-const newKenny = kenny.replace("Kenny", "Gameboy");
+kenny.splice(0, 1, "Gameboy")
+console.log(kenny)
+// Answer: https://www.javascripttutorial.net/javascript-array-splice/ (disregard below notes)
 // found this article online but still not getting the correct console.log
 // https://www.w3schools.com/jsref/jsref_replace.asp
 // 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code 187--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
-const newJimClark = jimClark.replace(186, ([1] ++1);
+jimClark[1] += 1;
+console.log(jimClark)
+//Answer: https://bobbyhadz.com/blog/javascript-increment-values-in-array
 // 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
-const newRyan = ryan.replace("Denver", "Gotham City");
+ryan.splice(2, 1, "Gotham City");
+console.log(ryan)
+// Answer: https://www.javascripttutorial.net/javascript-array-splice/
 // 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
-reuben.pop
+reuben.splice(-1)
+console.log(reuben)
 reuben.push("Chicago")
+console.log(reuben)
+// Answer: *removal of Durham w/ splie-1* https://stackoverflow.com/questions/19544452/remove-last-item-from-array
 // 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
-const newJimHaff = jimHaff.replace("All cities", ["New York", "Singapore", "Cumberland"]);
+jimHaff.splice(2, 3, "New York", "Sacremento", "Alexandria");
+console.log(jimHaff)
 // 6. passed on Bonus because of time constraints (t-Minus 8 minutes to start of class)
 
 
