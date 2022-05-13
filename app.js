1cd8fb76651fd6a4a56504c4cffc5fc50b5b8d35
idbic <dbicga@gmail.com>
2022-05-12 20:26:33 -0500

    Commit 6 - Yell at the ninja turtles answered

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //1. Easy Going
-let i = 0;
+//let i = 0;
 
 //for(let i = 1; i <= 20; i++) {
 //   console.log(i)
@@ -19,38 +19,49 @@ let i = 0;
 //}
 
 //4. FizzBuzz
-function fizzbuzz() {
-    for (let i = 1; i < 101; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz")
-    } else if (i % 3 === 0) {
-        console.log("Fizz")
-    } else if (i % 5 === 0) {
-        console.log("Buzz")
+//function fizzbuzz() {
+//  for (let i = 1; i < 101; i++) {
+//    if (i % 3 === 0 && i % 5 === 0) {
+//        console.log("FizzBuzz")
+//} else if (i % 3 === 0) {
+//     console.log("Fizz")
+//} else if (i % 5 === 0) {
+//  console.log("Buzz")
 
-    } else
-        console.log(i)
-}
-}
+// } else
+//   console.log(i)
+//}
+//}
 
-console.log(fizzbuzz());
+//console.log(fizzbuzz());
 
 //5.Getting to know you
 
-const kenny = ["Kenny", 1000, "Austin"];
-const jimHaff = ["Jim H", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const jimClark = ["Jim C", 186, "LA"];
-const ryan = ["Ryan", 65, "Denver"];
+//const kenny = ["Kenny", 1000, "Austin"];
+//const jimHaff = ["Jim H", 16, "All cities"];
+//const reuben = ["Reuben", 22, "Durham"];
+//const jimClark = ["Jim C", 186, "LA"];
+//const ryan = ["Ryan", 65, "Denver"];
 
-kenny[0] = "Gameboy";
-console.log(kenny[0]);
+//kenny[0] = "Gameboy";//
+//console.log(kenny[0]);
 
-jimClark[1] = 187;
-console.log(jimClark[1]);
+//jimClark[1] = 187;
+//console.log(jimClark[1]);
 
-ryan[2] = "Gotham City";
-console.log(ryan[2]);
+//ryan[2] = "Gotham City";
+//console.log(ryan[2]);
 
-jimHaff.splice(2, 1, 'Cleveland', 'Austin', 'Boulder');
-console.log(jimHaff)
+//jimHaff.splice(2, 1, 'Cleveland', 'Austin', 'Boulder');
+//console.log(jimHaff);
+
+
+// 6. Yell at the Ninja Turtles
+
+const nTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+
+
+for(let i = 0; i < nTurtles.length; i++) {
+    let turtle = console.log(nTurtles[i]);
+    console.log(toUpperCase(turtle))
+}
