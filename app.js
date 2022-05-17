4370936e0c06c015391b3478418b27069a2882f6
poojad2101 <davepooja2101@gmail.com>
2022-05-17 06:00:16 +0530

    Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -143,3 +143,12 @@ for (let i = 0; i < kristynsCloset.length; i++) {
 for (let i = 0; i < thomsCloset.length; i++) {
     console.log(thomsCloset[i]);
 }
+
+9. Multiples of 3 and 5
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+    if(i % 3 === 0 && i % 5 === 0)
+        sum += i;
+}
+
+console.log(sum);
